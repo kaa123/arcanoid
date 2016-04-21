bbb81c0779a381a16cd9fde2c5aca1d5147bd247
Alex <alexandra.arturovna@gmail.com>
2016-04-21 20:06:40 +0600

    'game'

--- namescript.js
+++ namescript.js
@@ -8,7 +8,16 @@ document.addEventListener("DOMContentLoaded",
 					bally:0,
 					boardPosition:0,
 					boardWidth:100,
-					fieldWidth:600
+					boardHeight: 10,
+					fieldWidth:600,
+					fieldHeight: 400,
+					ballWidth: 50,
+					ballHeight: 50,
+					step:25,
+					interval:10,
+					dirx:1,
+					diry:1,
+					intervalId:null
 
 				}
 			},
@@ -21,7 +30,30 @@ document.addEventListener("DOMContentLoaded",
 						this.boardPosition=this.fieldWidth-this.boardWidth;
 
 					}
+				},
+				onClick: function(){
+					var self=this;
+					if(this.intervalId===null){
+						this.intervalId = setInterval(function(){
+							self.ballx+=self.step*self.dirx;
+							self.bally+=self.step*self.diry;
+							if(self.ballx>=self.fieldWidth-self.ballWidth||self.ballx<=0){
+								self.dirx=self.dirx*(-1);
+							}if(self.bally<=0){
+								self.diry=self.diry*(-1);
+							}if(self.bally>=self.fieldHeight-self.ballHeight){
+								var ballPosition=self.ballx+self.ballWidth/2;
+								if(ballPosition>=self.boardPosition && ballPosition<=self.boardPosition+self.boardWidth){
+									self.diry=self.diry*(-1);
+								}else{
+									clearInterval(self.intervalId);
+								}
+							}
+						},100)
+					}
+					
 				}
+
 			}
 			
 
